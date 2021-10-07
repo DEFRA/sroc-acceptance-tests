12d586948c98fbbf981a1b3d6785d6d9ffd412fa
Stuart Adair <43574728+StuAA78@users.noreply.github.com>
2021-10-07 14:14:12 +0100

    Param change and small refactor
    We change the `Key` param (which is not really the key, as that also includes the filename, but the remote path) to be `fullPath`. We also do a small refactor of how w chain each `.then()`

--- cypress/integration/common/import.js
+++ cypress/integration/common/import.js
@@ -2,14 +2,16 @@ import { Given } from 'cypress-cucumber-preprocessor/steps'
 // import { S3Client, PutObjectCommand } from '@aws-sdk/client-s3'
 
 Given('I import the file {string}', (filename) => {
-  cy.fixture(filename).then(Body => {
-    cy.task('s3Upload', {
-      Body,
-      Bucket: Cypress.env('S3_BUCKET'),
-      Key: Cypress.env('S3_KEY'),
-      filename
-    }).then(fullPath => {
+  cy.fixture(filename)
+    .then(Body => {
+      cy.task('s3Upload', {
+        Body,
+        Bucket: Cypress.env('S3_BUCKET'),
+        remotePath: Cypress.env('S3_PATH'),
+        filename
+      })
+    })
+    .then(fullPath => {
       cy.log(`${fullPath} uploaded`)
     })
-  })
 })
--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -63,15 +63,15 @@ module.exports = (on, config) => {
   on('file:preprocessor', cucumber())
 
   on('task', {
-    s3Upload ({ Body, Bucket, Key, filename }) {
+    s3Upload ({ Body, Bucket, remotePath, filename }) {
       const s3 = new AWS.S3({ apiVersion: '2006-03-01' })
-      const fullKey = path.join(Key, filename)
-      s3.putObject({ Bucket, Key: fullKey, Body }, (err, data) => {
+      const Key = path.join(remotePath, filename)
+      s3.putObject({ Bucket, Key, Body }, (err, data) => {
         if (err) {
           throw err
         }
       })
-      return path.join(Bucket, fullKey)
+      return path.join(Bucket, Key)
     }
   })
 
